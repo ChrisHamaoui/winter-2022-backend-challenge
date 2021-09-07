27ae7a2249a39567061aad9c665ce855f3f0d60a
Christian <chrishamaoui@hotmail.com>
2021-09-07 18:28:38 -0400

    Initialize MongodB cluster and add .env

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +13,20 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-// Server static assets if in production
+// MongoDB connection credentials
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_CONNECTION_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.zwndv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// MongoDB app connection
+mongoose
+  .connect(DB_CONNECTION_URL, {})
+  .then(() =>
+    app.listen(port, () => console.log(`Server running on port: ${port}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// Server static assets if in production (Heroku)
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static('client/build'));
 
@@ -18,12 +34,3 @@ app.use(cors());
 //     response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 //   });
 // }
-
-// GET method route
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage');
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
