962a33937df9305d7aeb56cbcb478b96f04700b9
Christian <chrishamaoui@hotmail.com>
2021-09-07 21:55:27 -0400

    Build AppBar and update app styling

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
+// Custom JSX components imports
+import Header from './Header';
+import Footer from './Footer';
+
+// material-ui imports
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#004c3f',
+      secondary: '#9e9e9e',
+    },
+    background: {
+      default: '#fbf7ed',
+    },
+    text: {
+      primary: '#004c3f',
+      secondary: '#9e9e9e',
+    },
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Footer />
+      </ThemeProvider>
+    </div>
+  );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+// material-ui imports
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import ImageAspectRatioIcon from '@material-ui/icons/ImageAspectRatio';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import EditIcon from '@material-ui/icons/Edit';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    color: '#004c3f',
+    marginLeft: 8,
+    marginRight: 40,
+    fontWeight: 700,
+  },
+  logo: {
+    position: 'relative',
+    top: theme.spacing.unit,
+    marginRight: '5px',
+  },
+  rightToolbar: {
+    marginLeft: 'auto',
+    marginRight: -12,
+  },
+}));
+
+function Header() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position='static' color='transparent' elevation={4}>
+        <Toolbar>
+          <Typography variant='h5' className={classes.title}>
+            <ImageAspectRatioIcon fontSize='large' className={classes.logo} />
+            Image Gallery
+          </Typography>
+
+          <section className={classes.rightToolbar}>
+            <Tooltip title='Add'>
+              <IconButton color='inherit' title='Add'>
+                <AddCircleIcon />
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title='Delete'>
+              <IconButton color='inherit' aria-label='delete'>
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title='Edit'>
+              <IconButton color='inherit' aria-label='edit'>
+                <EditIcon />
+              </IconButton>
+            </Tooltip>
+          </section>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
