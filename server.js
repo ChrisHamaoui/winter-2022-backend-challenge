90c0f4518f478e2e4a79861992905011bf98746b
Christian <chrishamaoui@hotmail.com>
2021-09-10 18:55:38 -0400

    Create MongoDB schema

--- server.js
+++ server.js
@@ -8,12 +8,19 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '16mb', extended: true }));
 app.use(express.urlencoded({ limit: '16mb', extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection credentials
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_CONNECTION_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.zwndv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// MongoDB image schema
+const imageSchema = new mongoose.Schema({
+  name: String,
+  src: String,
+});
+
+const Image = mongoose.model('Image', imageSchema);
 
 // MongoDB connection
 mongoose
